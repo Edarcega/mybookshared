d5e8db8f8757c0cdbb28329a651c544752162092
Edarcega <edimar.eds@gmail.com>
2022-05-28 10:54:44 -0300

    Projeto criado com a primeira classe

---
+++ src/main/java/com/edstecno/bookshare/MybooksharedApplication.java
@@ -0,0 +1,13 @@
+package com.edstecno.bookshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybooksharedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybooksharedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edstecno/bookshare/MybooksharedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edstecno.bookshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybooksharedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
