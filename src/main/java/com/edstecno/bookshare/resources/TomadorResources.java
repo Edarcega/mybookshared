ed6d8114a8421fba857e66467bccbb264d899ae8
Edarcega <edimar.eds@gmail.com>
2022-06-03 20:00:26 -0300

    Criado resource de tomadores, consulta geral e consulta pelo id

--- src/main/java/com/edstecno/bookshare/resources/TomadorResources.java
+++ src/main/java/com/edstecno/bookshare/resources/TomadorResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class TomadorResources {
 	public ResponseEntity<List<Tomador>> findAll() {
 		List<Tomador> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Tomador> findById(@PathVariable Long id){
+		Tomador obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
